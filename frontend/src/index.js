29c6b6c465c9fca5ea291cb3d0b03b8464e5578f
PeterGanZW <zhuowei18@gmail.com>
2019-11-06 18:30:53 +1100

    try resolve merge conflict

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,11 +1,16 @@
 import ReactDOM from "react-dom";
+import ErrorBoundary from "./ErrorBoundary";
 import "./index.css";
 import * as serviceWorker from "./serviceWorker";
 import HomePage from "./homepage/HomePage";
-import App from "./homepage/App";
 import React from "react";
 
-ReactDOM.render(<App />, document.getElementById("root"));
+ReactDOM.render(
+  <ErrorBoundary>
+    <HomePage />
+  </ErrorBoundary>,
+  document.getElementById("root")
+);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
